c0345b94c0190c6a7a3e3e98673eacc84d31d7e8
vodoprovod <olegbobrovnik228@gmail.com>
2025-02-02 14:25:22 +0200

    add sth

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,67 @@
-import Profile from "./components/Profile/Profile";
-import FriendList from "./components/FriendList/FriendList";
-import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
-
-import userData from "./data/userData.json";
-import friends from "./data/friends.json"
-import transaction from  "./data/transaction.json"
-
+import { useState } from "react";
 import "./App.css";
+import { useEffect } from "react";
+import { fetchDate } from "./services/api";
+import ImageGalery from "./components/ImageGalery/ImageGalery";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import ImageModal from "./components/ImageModal/ImageModal";
+import SearchBar from "./components/SearchBar/SearchBar";
 
 function App() {
+  const [photo, setPhoto] = useState([]);
+  const [isload, setIsLoad] = useState(false);
+  const [error, setError] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [query, setQuery] = useState("");
+  const [page, setPage] = useState(1);
+  const [selectedPhoto, setSelectedPhoto] = useState(null);
+
+  useEffect(() => {
+    if (!query) return;
+    const handleDate = async () => {
+      try {
+        setIsLoad(true);
+        setError(false);
+        const res = await fetchDate(query, page);
+        setPhoto((prev) => [...prev, ...res]);
+      } catch {
+        setError(true);
+      } finally {
+        setIsLoad(false);
+      }
+    };
+    handleDate();
+  }, [query, page]);
+
+  const getTypeDate = (item) => {
+    setQuery(item);
+    setPhoto([]);
+    setPage(1);
+  };
+
+  const openModal = (image) => {
+    setSelectedPhoto(image);
+    setIsOpen(true);
+  };
+  const closeModal = () => {
+    setIsOpen(false);
+    setSelectedPhoto(null);
+  };
   return (
     <>
-      <Profile
-        username={userData.username}
-        tag={userData.tag}
-        location={userData.location}
-        avatar={userData.avatar}
-        stats={userData.stats}
+      <SearchBar getTypeDate={getTypeDate} />
+      <ImageGalery photo={photo} onClick={openModal} />
+      {photo.length > 0 && <LoadMoreBtn setPage={setPage} />}
+      {photo.length === 0 && <p>List is empty! Please search sth:)</p>}
+      <ImageModal
+        photo={selectedPhoto}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
       />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transaction} />
+      {isload && <Loader />}
+      {error && <ErrorMessage />}
     </>
   );
 }
---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import s from "./SearchBar.module.css";
+const SearchBar = ({ getTypeDate }) => {
+  const [cur, setCur] = useState("");
+  const handleForm = (e) => {
+    e.preventDefault();
+    getTypeDate(cur);
+  };
+  return (
+    <header className={s.header}>
+      <form className={s.form} onSubmit={handleForm}>
+        <input
+          className={s.input}
+          value={cur}
+          onChange={(e) => setCur(e.target.value)}
+          type="text"
+          placeholder="Search images and photos"
+        />
+        <button className={s.button} type="submit">
+          Search
+        </button>
+      </form>
+    </header>
+  );
+};
+
+export default SearchBar;
